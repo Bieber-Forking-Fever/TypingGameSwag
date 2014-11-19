b554b0d74320a31fcfc6075f5b583d7edc5a4710
Bieber-Forking-Fever <asfiohqwtgpowh@gmail.com>
2014-11-19 15:11:49 -0600

    Got the Numbers set +

---
+++ Typing Game.py
@@ -0,0 +1,94 @@
+from Tkinter import *
+
+class MyApp:
+	def __init__(self, parent):
+		self.myContainer1 = Frame(parent)
+		self.myContainer1.pack()
+		
+                self.label1 = Label(root, text="a", bg='red')
+                self.label1.pack()
+
+                self.label2 = Label(root, text="b", bg='red')
+                self.label2.pack()
+
+                self.label3 = Label(root, text="c", bg='red')
+                self.label3.pack()
+
+                self.label4 = Label(root, text="d", bg='red')
+                self.label4.pack()
+
+                self.label5 = Label(root, text="e", bg='red')
+                self.label5.pack()
+
+                self.label6 = Label(root, text="f", bg='red')
+                self.label6.pack()
+
+                self.label7 = Label(root, text="g", bg='red')
+                self.label7.pack()
+
+                self.label8 = Label(root, text="h", bg='red')
+                self.label8.pack()
+
+                self.label9 = Label(root, text="i", bg='red')
+                self.label9.pack()
+
+                self.label10 = Label(root, text="j", bg='red')
+                self.label10.pack()
+
+                self.label11 = Label(root, text="k", bg='red')
+                self.label11.pack()
+
+                self.label12 = Label(root, text="l", bg='red')
+                self.label12.pack()
+
+                self.label13 = Label(root, text="m", bg='red')
+                self.label13.pack()
+
+                self.label14 = Label(root, text="n", bg='red')
+                self.label14.pack()
+
+                self.label15 = Label(root, text="o", bg='red')
+                self.label15.pack()
+
+                self.label16 = Label(root, text="p", bg='red')
+                self.label16.pack()
+
+                self.label17 = Label(root, text="q", bg='red')
+                self.label17.pack()
+
+                self.label18 = Label(root, text="r", bg='red')
+                self.label18.pack()
+
+                self.label19 = Label(root, text="s", bg='red')
+                self.label19.pack()
+
+                self.label20 = Label(root, text="t", bg='red')
+                self.label20.pack()
+
+                self.label21 = Label(root, text="u", bg='red')
+                self.label21.pack()
+
+                self.label22 = Label(root, text="v", bg='red')
+                self.label22.pack()
+
+                self.label23 = Label(root, text="w", bg='red')
+                self.label23.pack()
+
+                self.label24 = Label(root, text="x", bg='red')
+                self.label24.pack()
+
+                self.label25 = Label(root, text="y", bg='red')
+                self.label25.pack()
+
+                self.label26 = Label(root, text="z", bg='red')
+                self.label26.pack()
+
+
+
+
+
+	
+		
+root = Tk()
+myapp = MyApp(root)
+root.mainloop()
